5bfdf981157fc1e9af0cacaecec17f51996997ca
Nikhil Dhage <nikhil.dhage@gmail.com>
2023-10-27 04:13:31 -0400

    Added sql code to create saved locations table

--- sqlschema.sql
+++ sqlschema.sql
@@ -3,3 +3,11 @@ CREATE TABLE IF NOT EXISTS users (
     username TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS saved_locations (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER,
+    city TEXT NOT NULL,
+    state TEXT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
